609e6b564c5484d7c1fc30a61d28ae1918c27c13
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-01 19:55:48 +0800

    ran terraform fmt on main.tf

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
   versioning_configuration {
     enabled = true
-    
+
   }
   lifecycle_rule {
     id      = "expire-objects"
