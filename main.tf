98e385daa514a72871585fc37eee1e3b573581b3
vrushalibavare <vrushali.bavare@gmail.com>
2025-07-01 19:14:49 +0800

    modified interpolation statements and added required version block

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
-provider "aws" {
-  region = "ap-southeast-1"
-}
-
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket = "vrush-tfstate-bucket"   #Change this
     key    = "vrush-s3-tf-ci.tfstate" #Change this
@@ -10,13 +12,21 @@ terraform {
   }
 }
 
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+
 data "aws_caller_identity" "current" {}
 
 locals {
-  name_prefix = split("/", "${data.aws_caller_identity.current.arn}")[1]
-  account_id  = data.aws_caller_identity.current.account_id
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
+account_id  = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 }
+
+
+
